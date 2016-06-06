848556a591b84199cc1d86ca0472e024524c0596
nikhilgudla <nikhilgudla@live.com>
2016-06-06 15:11:40 -0700

    Update Transaction.java
    Added method to be used for new feature maxi savings rate

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,31 @@
 package com.abc;
 
-import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 
+ * This is Transaction Object, Constructor takes in transaction amount as
+ * parameter
+ * 
+ * @author Nikhil
+ *
+ */
 public class Transaction {
-    public final double amount;
+	public final double amount;
 
-    private Date transactionDate;
+	private Date transactionDate;
 
-    public Transaction(double amount) {
-        this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
-    }
+	public Transaction(double amount) {
+		this.amount = amount;
+		this.transactionDate = DateProvider.getInstance().now();
+	}
 
+	public boolean occuredInPastTenDays() {
+		Date now = new Date();
+		// 10 Days of milliseconds 1000 * 60 * 60 *24 * 10 = 864000000
+		if (transactionDate.getTime() >= now.getTime() - 864000000) {
+			return true;
+		}
+		return false;
+	}
 }
