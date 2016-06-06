f5a0669fcbe550ab6a21caea52205d7224ab1e5b
nikhilgudla <nikhilgudla@live.com>
2016-06-06 15:12:14 -0700

    Update DateProvider.java
    Refractored

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -3,16 +3,32 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * Provides date instance or current date
+ * 
+ * @author Nikhil
+ *
+ */
 public class DateProvider {
-    private static DateProvider instance = null;
+	private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	/**
+	 * This method provides new DateProvider instance
+	 * 
+	 * @return DateProvider
+	 */
+	public static DateProvider getInstance() {
+		if (instance == null)
+			instance = new DateProvider();
+		return instance;
+	}
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	/**
+	 * This method provides the date now
+	 * 
+	 * @return Date
+	 */
+	public Date now() {
+		return Calendar.getInstance().getTime();
+	}
 }
