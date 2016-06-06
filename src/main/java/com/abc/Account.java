1f8b4167584c6fc2ac9a1129c58455b855ec1315
nikhilgudla <nikhilgudla@live.com>
2016-06-06 15:08:45 -0700

    Update Account.java
    Cleaned up super savings case, Added new feature to accrue interest daily, Added new feature for Maxi Savings interest rate. Refractored.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -5,69 +5,110 @@ import java.util.List;
 
 public class Account {
 
-    public static final int CHECKING = 0;
-    public static final int SAVINGS = 1;
-    public static final int MAXI_SAVINGS = 2;
+	public static final int CHECKING = 0;
+	public static final int SAVINGS = 1;
+	public static final int MAXI_SAVINGS = 2;
 
-    private final int accountType;
-    public List<Transaction> transactions;
+	private final int accountType;
+	public List<Transaction> transactions;
 
-    public Account(int accountType) {
-        this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
-    }
+	public Account(int accountType) {
+		this.accountType = accountType;
+		this.transactions = new ArrayList<Transaction>();
+	}
 
-    public void deposit(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("amount must be greater than zero");
-        } else {
-            transactions.add(new Transaction(amount));
-        }
-    }
+	public void deposit(double amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException(
+					"amount must be greater than zero");
+		} else {
+			transactions.add(new Transaction(amount));
+		}
+	}
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
-    }
-}
+	public void withdraw(double amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException(
+					"amount must be greater than zero");
+		} else {
+			transactions.add(new Transaction(-amount));
+		}
+	}
 
-    public double interestEarned() {
-        double amount = sumTransactions();
-        switch(accountType){
-            case SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
-//            case SUPER_SAVINGS:
-//                if (amount <= 4000)
-//                    return 20;
-            case MAXI_SAVINGS:
-                if (amount <= 1000)
-                    return amount * 0.02;
-                if (amount <= 2000)
-                    return 20 + (amount-1000) * 0.05;
-                return 70 + (amount-2000) * 0.1;
-            default:
-                return amount * 0.001;
-        }
-    }
+	public double interestEarned() {
+		double amount = sumTransactions();
 
-    public double sumTransactions() {
-       return checkIfTransactionsExist(true);
-    }
+		if (amount < 0)
+			return 0;
 
-    private double checkIfTransactionsExist(boolean checkAll) {
-        double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.amount;
-        return amount;
-    }
+		switch (accountType) {
+		case SAVINGS:
+			if (amount <= 1000)
+				return amount * 0.001;
+			else
+				return 1 + (amount - 1000) * 0.002;
+		case MAXI_SAVINGS:
+			if (this.noWithdrawalsInPastTenDays()) {
+				return amount * .05;
+			}
+			return amount * .01;
+		case CHECKING:
+			return amount * 0.001;
+		default:
+			return 0;
+		}
+	}
 
-    public int getAccountType() {
-        return accountType;
-    }
+	/**
+	 * Interest is accrued daily as per the new feature including weekends
+	 * 
+	 * @return interest
+	 */
+	public double interestEarnedPerDay() {
+		double accrueDayCount = 365;
+		double amount = sumTransactions();
 
+		// If amount is 0, interest is defaulted to 0.
+		if (amount < 0)
+			return 0;
+
+		switch (accountType) {
+		case SAVINGS:
+			if (amount <= 1000)
+				return amount * 0.001 / accrueDayCount;
+			else
+				return 1 + (amount - 1000) * 0.002 / accrueDayCount;
+		case MAXI_SAVINGS:
+			if (this.noWithdrawalsInPastTenDays()) {
+				return amount * .05 / accrueDayCount;
+			}
+			return amount * .01 / accrueDayCount;
+		case CHECKING:
+			return amount * 0.001 / accrueDayCount;
+		default:
+			return 0;
+		}
+	}
+
+	public double sumTransactions() {
+		return checkIfTransactionsExist(true);
+	}
+
+	private double checkIfTransactionsExist(boolean checkAll) {
+		double amount = 0.0;
+		for (Transaction transaction : transactions)
+			amount += transaction.amount;
+		return amount;
+	}
+
+	public int getAccountType() {
+		return accountType;
+	}
+
+	public boolean noWithdrawalsInPastTenDays() {
+		for (Transaction t : transactions)
+			if (t.occuredInPastTenDays())
+				return false;
+		return true;
+	}
 }
